854ae383b057544487aa3d956241b6ddf14aad17
Eddie Szabatin <eddieszabatin@gmail.com>
2020-11-19 17:04:51 -0800

    Add files via upload
    a little easy math quiz, 2nd ever project.

---
+++ math quiz.py
@@ -0,0 +1,34 @@
+print("Hi, this is a math quiz.")
+print("what's 9 + 10?")
+
+ans1 = input()
+
+if ans1 == '19':
+    print("thats correct!")
+if ans1 !='19':
+    print("oh no, thats wrong")
+
+print("what's 12 x 12?")
+
+ans2 = input()
+
+if ans2 == '144':
+    print('thats right!')
+if ans2 !='144':
+    print("oh no, thats wrong")
+
+print("what's 48 / 8?")
+
+ans3 = input()
+
+if ans3 == '6':
+    print("wow you're on a roll!")
+
+print('what is the median of this data set?')
+for x in range(0, 10, 1):
+    print(x)
+ans4 = input()
+
+if ans4 == ('4'):
+    print("good job! you passed.")
+
